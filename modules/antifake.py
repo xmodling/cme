5ca1515fefbe12e354de0ca1754ce4fa2976d5d9
xmodling <70622770+xmodling@users.noreply.github.com>
2021-04-27 13:54:54 +0300

    Create antifake.py

---
+++ modules/antifake.py
@@ -0,0 +1,15 @@
+import json, requests
+class AntiFake:
+    def fake_filter(event, token):
+        print("start")
+        user = event.__dict__["info"]["user_id"]
+        print(user)
+        count = requests.get(
+            'https://api.vk.com/method/{method}?{params}&access_token={token}&v=5.95'.format(method = 'users.get',params = f'user_id={user}&fields=counters',token = token)).json()['response'][0]['counters']['friends']
+        if count < 10:
+            requests.get(
+            'https://api.vk.com/method/{method}?{params}&access_token={token}&v=5.95'.format(
+            method = 'messages.send',
+            params = f'random_id=0&peer_id={event.peer_id}&message=Подозрительный [id{user}|аккаунт] (меньше 10 друзей). Исключаю...',
+            token = token))
+            requests.get('https://api.vk.com/method/messages.send?{params}&access_token={token}&v=5.95'.format(params = f'random_id=0&peer_id={event.peer_id}&message=!ban @id{user}',token = token))
