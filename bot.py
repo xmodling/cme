f62ff7f2e48c765c4e7d4e259cc3d2f9e8dc7d8d
xmodling <70622770+xmodling@users.noreply.github.com>
2021-04-27 18:37:36 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -227,7 +227,7 @@ for event in longpoll.listen():
                   response = requests.get('https://api.vk.com/method/messages.send?{params}&access_token={token}&v=5.95'.format(
                           params = f'peer_id={event.peer_id}&random_id=0&message={quote}', token = token))
               if '/pig' in event.text.lower() and int(event.__dict__['from']) in nfcusers:
-                  threading.Thread(target=burp.BurpManager.burp_sender, args=[token, event, 90]).start()
+                  threading.Thread(target=burp.BurpManager.burp_sender, args=[token, event, 30]).start()
               if event.text.lower() == '.nfcstatus':
                       text = event.__dict__
                       checkid = text['from']
