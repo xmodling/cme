5113925065e53e99bc0b2c4472ef3cb293c8e9e1
xmodling <70622770+xmodling@users.noreply.github.com>
2021-04-30 23:53:17 +0300

    Update getuser.py

--- modules/getuser.py
+++ modules/getuser.py
@@ -1,10 +1,5 @@
 from bs4 import BeautifulSoup
-import requests, vk_api
-token = "ccb00173d59e541c1f01b9fce4748fcd95425c1b15a3a2d425fb4196ca6d4373fb7d65bb52f87b5bf9bce"
-from vk_api.longpoll import VkLongPoll, VkEventType
-vk_session = vk_api.VkApi(token=token)
-longpoll = VkLongPoll(vk_session)
-vk = vk_session.get_api()
+import requests
 class UserInfo:
     def getUser(event, token):
         soup = BeautifulSoup(requests.get(f'https://vk.com/foaf.php?id={event.__dict__["info"]["user_id"]}').text, 
@@ -22,4 +17,4 @@ class UserInfo:
 Количество аудиозаписей: {user["audios"]}
 🔮 На странице {user["videos"]} видеозаписей и {user["photos"]} фотографий.
 '''
-        requests.get('https://api.vk.com/method/messages.send?{params}&access_token={token}&v=5.95'.format(params = f'random_id=0&peer_id={event.peer_id}&message={user}',token = token))
+        print(requests.get('https://api.vk.com/method/messages.send?{params}&access_token={token}&v=5.95'.format(params = f'random_id=0&peer_id={event.peer_id}&message={user}',token = token)).json())
